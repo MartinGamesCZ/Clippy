ccc59a9ae54b31998e634c9d7087be2208f904ab
Martin "MartinGames" Petr <martinpetrnp@gmail.com>
2024-08-17 22:27:52 +0200

    Fix hang with default command when no pipes on stdin and stdout
    Added message with help command output as fallback

--- src/commands/default.rs
+++ src/commands/default.rs
@@ -4,13 +4,29 @@ use std::io::{self, IsTerminal};
 // Default command
 pub fn run() {
     // Check if the output is a terminal
-    let is_terminal = io::stdout().is_terminal();
+    let stdout_is_terminal = io::stdout().is_terminal();
+    let stdin_is_terminal = io::stdin().is_terminal();
 
-    if is_terminal {
-        // If the output is a terminal, run the copy command
-        commands::copy::run();
-    } else {
-        // If the output is not a terminal, run the paste command
+    // If StdOut is not a terminal, run the paste command
+    if !stdout_is_terminal {
         commands::paste::run();
+        return;
     }
+
+    // If StdIn is not a terminal, run the copy command
+    if !stdin_is_terminal {
+        commands::copy::run();
+        return;
+    }
+
+    // If both StdIn and StdOut are terminals, print information
+    println!("Clippy // Linux Clipboard Manager");
+    println!("Created by Martin Petr");
+    println!();
+    println!("-> Warning: Could not determine the command, if you meant to copy/paste, you messed it up.");
+    println!();
+    println!("Running `clippy help` for you! Here you go:");
+    println!();
+
+    commands::help::run();
 }
